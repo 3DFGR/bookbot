997047833f57dc222245cc1c1742663b5e0c1e0c
3DFGR <shevchenko3d@gmail.com>
2024-07-27 00:15:22 +0300

    added book aggregator

---
+++ main.py
@@ -0,0 +1,51 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sorted_chars(chars: dict[str, int]) -> list:
+    result = []
+    for key in chars:
+        if key.isalpha():
+            result.append({"name": key, "count": chars[key]})
+    result.sort(reverse=True, key=lambda x: x["count"])
+    return result
+
+
+def count_chars(text: str) -> dict: 
+    result: dict = {}
+    for char in text:
+        char_lower = char.lower()
+        if char_lower in result:
+            result[char_lower] += 1
+        else:
+            result[char_lower] = 1
+    return result
+
+
+def sasha_molodets(path):
+    text = get_book_text(path)
+    words = count_words(text)
+    chars = sorted_chars(count_chars(text))
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{words} words found in the document")
+    print()
+    for chardict in chars:
+        print(f"The {chardict["name"]} character was found {chardict["count"]} times")
+
+    print("--- End report ---")
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    sasha_molodets(book_path)
+
+
+if __name__ == "__main__":
+    main()
